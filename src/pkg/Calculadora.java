a66b6708dcacb9fa4cb1b4ce807476ce5e12bccd
Agustin <usuario@pc21-082>
2025-10-06 17:16:34 +0200

    TDD paso 3. Refactorizar código.

--- src/pkg/Calculadora.java
+++ src/pkg/Calculadora.java
@@ -4,22 +4,22 @@ public class Calculadora {
 
 	public static Integer suma(int i, int j) {
 		// TODO Auto-generated method stub
-		return 5;
+		return i+j;
 	}
 
 	public static Integer resta(int i, int j) {
 		// TODO Auto-generated method stub
-		return 5;
+		return i-j;
 	}
 
 	public static Integer multiplicar(int i, int j) {
 		// TODO Auto-generated method stub
-		return 6;
+		return i*j;
 	}
 
 	public static Integer dividir(int i, int j) {
 		// TODO Auto-generated method stub
-		return 2;
+		return i/j;
 	}
 
 }
