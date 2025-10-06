089ef4d121a9af830ec6f77ec26a40a2efd6841c
usuario <usuario@pc21-082>
2025-10-06 17:39:09 +0200

    2º Test Cuenta correcto

---
+++ src/pkg/Cuenta.java
@@ -0,0 +1,32 @@
+package pkg;
+
+public class Cuenta {
+
+	String numero;
+	String titular;
+	double saldo;
+	
+	public Cuenta(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public void ingresar(double i) {
+		// TODO Auto-generated method stub
+		this.saldo = 500;
+	}
+
+	public void retirar(double i) {
+		// TODO Auto-generated method stub
+		this.saldo = -500.0;
+	}
+	
+	public double getSaldo() {
+		// TODO Auto-generated method stub
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+}
